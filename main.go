ae7fff297478fda3f945399ad6e601d979d6cfab
yuya okada <yuya.okada527@gmail.com>
2020-05-16 23:39:42 +0900

    add arrays

--- main.go
+++ main.go
@@ -3,16 +3,26 @@ package main
 import "fmt"
 
 func main() {
-	// string型
-	s := "string"
-	fmt.Println(s)
+	// 配列
+	a := [5]int{1, 2, 3, 4, 5}
+	fmt.Printf("%v\n", a)
 
-	// raw string
-	rs := `
-	you can write multi line string like this!!
-	Cool!!
-	But this does not support tab alignment and escape for align.
-	`
-	fmt.Println(rs)
+	// 参照
+	fmt.Println(a[0])
+	fmt.Println(a[1])
+	fmt.Println(a[2])
+	fmt.Println(a[3])
+	fmt.Println(a[4])
 
+	// 初期値
+	var b [5]int
+	fmt.Println(b)
+
+	// 要素数の省略
+	c := [...]int{1, 2, 3}
+	fmt.Println(c)
+
+	// 要素の代入
+	c[1] = 0
+	fmt.Println(c)
 }
