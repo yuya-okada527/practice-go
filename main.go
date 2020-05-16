ea740fe48e2e0ff8198e275ae751b2e4186a9e1a
yuya okada <yuya.okada527@gmail.com>
2020-05-16 15:11:11 +0900

    add var def

--- main.go
+++ main.go
@@ -1,6 +1,36 @@
 package main
 
+import (
+	"fmt"
+)
+
+// パッケージ変数
+var n = 10
+
+// 型推論はできない
+// n := 10
+
 func main() {
-	print("Hello, World!\n")
-	println("Hello, World!")
+	// 1行コメント
+	var i int = 1
+	fmt.Println(i)
+
+	/*
+		複行コメント
+	*/
+	var j, k int = 2, 3
+	fmt.Println(j, k)
+
+	var (
+		a = 1
+		b = true
+	)
+
+	fmt.Println(a, b)
+
+	p := 1
+
+	fmt.Println(p)
+
+	fmt.Printf("n=%d\n", n+1)
 }
