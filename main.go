bfdb373b5e35fd6cc4cb550d13fd10bc5d5b4cd1
yuya okada <yuya.okada527@gmail.com>
2020-05-17 00:05:49 +0900

    add error handling

--- main.go
+++ main.go
@@ -1,25 +1,13 @@
 package main
 
-import "fmt"
-
 func main() {
-	// function
-	fmt.Println(plus(1, 2))
-	hello()
-	fmt.Println(divmod(5, 3))
-
-	a, _ := divmod(17, 4)
-	fmt.Println(a)
+	//
 }
 
-func plus(x, y int) int {
-	return x + y
-}
+func div(a, b int) (int, int) {
+	if b == 0 {
+		return 0, 1
+	}
 
-func hello() {
-	fmt.Println("Hello, Wolrd!")
-}
-
-func divmod(a, b int) (int, int) {
-	return a / b, a % b
+	return 0, a / b
 }
