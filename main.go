68a815e715e28ce65406e925597e91b6e0946a55
yuya okada <yuya.okada527@gmail.com>
2020-05-16 23:49:52 +0900

    add operator

--- main.go
+++ main.go
@@ -3,7 +3,26 @@ package main
 import "fmt"
 
 func main() {
-	// インターフェース
-	var x interface{}
-	fmt.Printf("%#v\n", x)
+	// operator
+	a := "hoge"
+	b := "fuga"
+	fmt.Println(a + " " + b)
+
+	fmt.Println(1 + 2)
+	fmt.Println(2 * 3)
+	fmt.Println(10 - 3)
+	fmt.Println(12 / 5)
+	fmt.Println(5 % 2)
+
+	n := 1
+	n += 2
+	n *= 3
+	n /= 4
+	n %= 5
+	fmt.Println(n)
+
+	fmt.Println(1 == 1)
+	fmt.Println(3 >= 2)
+	fmt.Println(5 < 2)
+	fmt.Println(4 != 2)
 }
