1cf56f6c25d32d9f24a8fd8e1ae17c20871a356e
yuya okada <yuya.okada527@gmail.com>
2020-05-31 16:56:03 +0900

    add package

--- main.go
+++ main.go
@@ -1,24 +1,8 @@
 package main
 
-import "fmt"
-
-const (
-	MON = iota
-	THE
-	WED
-	THR
-	FRY
-	SAT
-	SUN
-)
+import "foo"
 
 func main() {
-	// enum
-	fmt.Println(MON)
-	fmt.Println(THE)
-	fmt.Println(WED)
-	fmt.Println(THR)
-	fmt.Println(FRY)
-	fmt.Println(SAT)
-	fmt.Println(SUN)
+	// package
+	foo.DoSomething()
 }
